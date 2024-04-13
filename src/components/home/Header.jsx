cf2c0df6931ff8999f2a2bdbc357f3e5130a8625
Kingjohneycodey <kvngjohnny10@gmail.com>
2024-04-13 20:06:15 +0100

    first commit

--- src/components/home/Header.jsx
+++ src/components/home/Header.jsx
@@ -1,12 +1,39 @@
 import React from 'react'
+import { FaBell, FaHeart, FaHome } from 'react-icons/fa'
+import { FaCartShopping } from 'react-icons/fa6'
+import { FiSearch } from 'react-icons/fi'
+import profile from "../../assets/profile1.webp";
 
 const Header = () => {
   return (
     <header>
         <h1>Learning</h1>
 
-        <div>
-            <input type="text" />
+
+        <div className="search-bar">
+      <FiSearch className="search-icon" />
+      <input
+        type="text"
+        placeholder="Search courses here..."
+        className="search-input"
+      />
+    
+    </div>
+
+    <div className='header-links'>
+      <span>Categories</span>
+      <span>Become a Teacher</span>
+      <span>My learning</span>
+    </div>
+        <div className='header-aside'>
+            <FaBell className='header-icons' />
+            <FaCartShopping className='header-icons' />
+            <FaHeart className='header-icons' />
+            <img
+            src={profile}
+            alt="profile Image"
+            className="profile-image"
+          />
         </div>
     </header>
   )
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import Layout from '../components/home/Layout'
-
-import background from "../assets/background.jpg"
+import React from "react";
+import Layout from "../components/home/Layout";
+import background from "../assets/background.jpg";
+import '../styles/main.css'
 
 const HomePage = () => {
   return (
     <div>
-        <Layout>
-            <img src={background} alt=""  />
-
-            Homepage
-        </Layout>
+      <Layout>
+        <div className="container">
+          <img
+            src={background}
+            alt="Background Image"
+            className="background-image"
+          />
+          <div className="text-overlay">
+            <h1>Get the best Courses Here</h1><br />
+            <p>We have courses covering a whole lot of fields that you can try out</p>
+          </div>
+        </div>
+        Homepage
+      </Layout>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
